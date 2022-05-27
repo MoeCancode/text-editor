7457a948dc81cdbebf19ae7bd7960a7a210bfe51
M A Razvi <abuzarflw@gmail.com>
2022-05-26 20:12:30 -0700

    Add click event handler to install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,15 +1,23 @@
-const butInstall = document.getElementById("buttonInstall");
+const buttonInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // event handler for the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", function (event) {
     window.deferredPrompt = event;
-    butInstall.classList.toggle("hidden", false);
+    buttonInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async function() {
-
+// TODO: Implement a click event handler on the `buttonInstall` element
+buttonInstall.addEventListener("click", async function() {
+    const promptEvent = window.deferredPrompt;
+    if(!promptEvent){
+        return;
+    }
+    else {
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    buttonInstall.classList.toggle('hidden', true)
+}
 });
 
 // TODO: Add an handler for the `appinstalled` event
